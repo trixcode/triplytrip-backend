f398fd10e710d1acfd58883d3c4f5207c0474dad
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2019-06-22 12:22:10 +0600

    added paginate search

--- src/app/place/index.js
+++ src/app/place/index.js
@@ -7,31 +7,36 @@ const verifyToken = require('../../middleware/verifyToken');
 const config = require('../../config');
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb)=>{
+  destination: (req, file, cb) => {
     cb(null, config.uploadPath);
   },
-  filename: (req, file, cb) =>{
+  filename: (req, file, cb) => {
     cb(null, nanoid() + path.extname(file.originalname));
   }
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 const router = express.Router();
 
-//get places
-router.get('/', (req, res)=> {
-  Place.find({isActive: true})
+// get places
+router.get('/', function (req, res) {
+  var page = req.query.skip || 0;
+  var limit = req.query.limit || 50;
+  Place.find({ isActive: true })
     .populate('user')
     .populate('category')
     .populate('country')
     .populate('cities')
     .sort('-createDate')
+    .lean()
+    .skip(Number(page))
+    .limit(Number(limit))
     .then(result => res.send(result))
     .catch(() => res.sendStatus(404))
 });
 
 //post place
-router.post('/', [verifyToken, upload.single('mainImage', 'images')], (req,res)=>{
+router.post('/', [verifyToken, upload.single('mainImage', 'images')], (req, res) => {
   const place = new Place(req.body);
 
   if (req.file) place.mainImage = req.file.filename;
@@ -41,7 +46,7 @@ router.post('/', [verifyToken, upload.single('mainImage', 'images')], (req,res)=
     place.save()
       .then(result => res.send(result))
       .catch(() => res.sendStatus(400))
-  } else if (req.user.roles.name === 'user'){
+  } else if (req.user.roles.name === 'user') {
     place.save()
       .then(result => res.send(result))
       .catch(() => res.sendStatus(401))
@@ -50,17 +55,17 @@ router.post('/', [verifyToken, upload.single('mainImage', 'images')], (req,res)=
 
 
 //edit places
-router.patch('/:id', verifyToken, (req, res)=>{
+router.patch('/:id', verifyToken, (req, res) => {
   if (req.user.roles.name === 'admin' || req.user.roles.name === 'moderator') {
-    Place.findById(req.params.id, (err, place)=>{
-      place.set({...req.body, updateDate: new Date(), isModerated: true});
-      place.save((saveErr, updated)=>{
-        res.send({updated})
+    Place.findById(req.params.id, (err, place) => {
+      place.set({ ...req.body, updateDate: new Date(), isModerated: true });
+      place.save((saveErr, updated) => {
+        res.send({ updated })
       })
     })
-  } else if (req.user._id.equals(req.params.id)){
+  } else if (req.user._id.equals(req.params.id)) {
     Place.find(req.params.id, (err, place) => {
-      place.set({...req.body, updateDate: new Date(), isModerated: true});
+      place.set({ ...req.body, updateDate: new Date(), isModerated: true });
       place.save((saveErr, updated) => {
         res.send(updated)
       })
@@ -73,11 +78,11 @@ router.patch('/:id', verifyToken, (req, res)=>{
 
 // All edit places
 router.put('/:id', (req, res) => {
-  if (req.file){
+  if (req.file) {
     place.mainImage = req.file.filename
   }
-  if (req.user.roles.name === 'admin' || req.user.roles.name === 'moderator'){
-    Place.findById(req.params.id, (err, place)=>{
+  if (req.user.roles.name === 'admin' || req.user.roles.name === 'moderator') {
+    Place.findById(req.params.id, (err, place) => {
       place.category = req.body.category;
       place.user = req.body.user;
       place.name = req.body.name;
@@ -113,7 +118,7 @@ router.put('/:id', (req, res) => {
           place.rating = req.body.rating;
           place.updatedDate = new Date();
           place.save((saveErr, updatedUser) => {
-            res.send({updatedUser});
+            res.send({ updatedUser });
           });
         })
       } else {
@@ -125,16 +130,16 @@ router.put('/:id', (req, res) => {
 
 //delete place by id
 router.delete('/:id', verifyToken, (req, res) => {
-  if (req.user.roles.name === 'admin' || req.user.roles.name === 'moderator'){
-    Place.findById(req.params.id, (err, place)=>{
-      place.remove(()=>{
+  if (req.user.roles.name === 'admin' || req.user.roles.name === 'moderator') {
+    Place.findById(req.params.id, (err, place) => {
+      place.remove(() => {
         res.send('Delete Place');
       })
     })
   } else if (req.user.roles.name === 'user') {
-    Place.findById(req.params.id, (err, place)=>{
-      if (req.user._id.equals(place.user)){
-        place.remove(()=>{
+    Place.findById(req.params.id, (err, place) => {
+      if (req.user._id.equals(place.user)) {
+        place.remove(() => {
           res.send('Delete Place');
         })
       } else {
